bd523a6b3a47b54fa410cfeb49f77bfa9aa1981b
Owos <owodunniabraham@gmail.com>
2022-07-23 01:41:05 +0000

    moved price and distance filling to the submit form action

--- bot/actions/actions.py
+++ bot/actions/actions.py
@@ -299,28 +299,9 @@ class SetPriceandDistance(Action):
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: Dict[Text, Any],
-        total_dist=total_dist,
-        
     ) -> List[Dict]:
         
         try:
-            regex_post = r"([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9][A-Za-z]?))))\s?[0-9][A-Za-z]{2})"
-            #getting slot values and extracting post codes
-            pick_add = tracker.get_slot("pickup_address")
-            match = re.search(regex_post, pick_add)
-            pick_post = pick_add[match.start():match.end()]
-
-            receive_add = tracker.get_slot("receiver_address")
-            match = re.search(regex_post, receive_add)
-            receive_post = receive_add[match.start():match.end()]
-            
-            #calculating distance
-            dist = pgeocode.GeoDistance('GB')
-            total_dist = dist.query_postal_code(pick_post, receive_post)/1000 +1 #in m
-            #setting price
-            price = 1 #in pounds
-            weight = tracker.get_slot("item_weight") #in kg
-            total_price = price * float(total_dist) * float(weight)
             dispatcher.utter_message(f"We can do this, it might cost you  £{total_price} to make a delivery")
             return [SlotSet("sending_distance", total_dist), SlotSet("sending_price", total_price)]
         except:
@@ -338,6 +319,8 @@ class ActionSubmitSendItem(Action):
         dispatcher: CollectingDispatcher,
         tracker: Tracker,
         domain: Dict[Text, Any],
+        total_dist,
+        total_price
     ) -> List[Dict]:
 
         item = tracker.get_slot("item")
@@ -364,13 +347,12 @@ class ActionSubmitSendItem(Action):
                 sender_name=sender_name,
                 receiver_name=receiver_name,
                 receiver_phone = receiver_phone,
-                sending_distance=sending_distance,
-                sending_price = sending_price
+                sending_distance=total_dist,
+                sending_price = total_price
                 
             )
         dispatcher.utter_message("Thanks, your answers have been recorded!") # message to the user about the form
-        return [SlotSet("sending_distance", total_dist), SlotSet("sending_price", total_price)]
-
+        return []
 
 # get tracking status
 class GetTrackingStatus(Action): 
